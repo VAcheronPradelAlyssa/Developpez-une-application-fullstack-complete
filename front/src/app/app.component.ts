770ab25e943f4f31991e7f63a421bdc844f4a4d3
VAcheronPradelAlyssa <vacheron.alyssa@aol.com>
2025-06-11 16:44:34 +0200

    page d'acceuil et initialisation

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  standalone: false, // This is not necessary unless you are using standalone components
 })
 export class AppComponent {
   title = 'front';
