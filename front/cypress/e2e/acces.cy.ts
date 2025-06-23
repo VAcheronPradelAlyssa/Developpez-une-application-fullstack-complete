07b0d4f8cd601986b70aa35e849c5a2d90d9f4dd
VAcheronPradelAlyssa <vacheron.alyssa@aol.com>
2025-06-23 18:34:36 +0200

    cypress acces aux pages

---
+++ front/cypress/e2e/acces.cy.ts
@@ -0,0 +1,68 @@
+describe('Tests d\'accès', () => {
+  beforeEach(() => {
+    cy.request('POST', 'http://localhost:8080/api/test/reset-db');
+  });
+
+  it('accès aux pages login et register sans être connecté', () => {
+    cy.visit('http://localhost:4200/login');
+    cy.contains('Connexion');
+    cy.visit('http://localhost:4200/register');
+    cy.contains('Inscription');
+  });
+
+  it('refuse l\'accès aux pages privées sans connexion', () => {
+    cy.visit('http://localhost:4200/post');
+    cy.url().should('include', '');
+    cy.visit('http://localhost:4200/profile');
+    cy.url().should('include', '');
+    cy.visit('http://localhost:4200/subject');
+    cy.url().should('include', '');
+    cy.visit('http://localhost:4200/subject/create');
+    cy.url().should('include', '');
+  });
+
+  it('autorise l\'accès aux pages privées après connexion', () => {
+    // Inscription + connexion
+    const username = 'accesuser' + Date.now();
+    const email = 'acces' + Date.now() + '@test.com';
+    cy.request('POST', 'http://localhost:8080/api/auth/register', {
+      username,
+      email,
+      password: 'Test1234!'
+    });
+    cy.visit('http://localhost:4200/login');
+    cy.get('input[formcontrolname="emailOrUsername"]').type(email);
+    cy.get('input[type="password"]').type('Test1234!');
+    cy.get('button[type="submit"]').click();
+    cy.url({ timeout: 10000 }).should('include', '/post');
+    cy.visit('http://localhost:4200/user-profile');
+    cy.url().should('include', '/user-profile');
+    cy.visit('http://localhost:4200/subject');
+    cy.url().should('include', '/subject');
+    cy.visit('http://localhost:4200/post');
+    cy.url().should('include', '/post');
+    cy.visit('http://localhost:4200/create-post');
+    cy.url().should('include', '/create-post');
+  });
+/*
+  it('redirige vers /post si déjà connecté et visite /login ou /register', () => {
+    // Inscription + connexion
+    const username = 'rediruser' + Date.now();
+    const email = 'redir' + Date.now() + '@test.com';
+    cy.request('POST', 'http://localhost:8080/api/auth/register', {
+      username,
+      email,
+      password: 'Test1234!'
+    });
+    cy.visit('http://localhost:4200/login');
+    cy.get('input[formcontrolname="emailOrUsername"]').type(email);
+    cy.get('input[type="password"]').type('Test1234!');
+    cy.get('button[type="submit"]').click();
+    cy.url({ timeout: 10000 }).should('include', '/post');
+    cy.visit('http://localhost:4200/login');
+    cy.url().should('include', '/post');
+    cy.visit('http://localhost:4200/register');
+    cy.url().should('include', '/post');
+  });
+*/
+});
